e544156924268ed5ce59ce110451d18d35f516cf
Seu Nome <caina.silva@sp.senai.br>
2026-06-08 10:37:07 -0300

    teste 1

--- src/ToDo.Api/Controllers/TodoController.cs
+++ src/ToDo.Api/Controllers/TodoController.cs
@@ -48,8 +48,8 @@ public class TodoController : ControllerBase
                 Priority = createdItem.Priority
             };
             
-            return NoContent();
-            //return Created($"/tasks/{resultDto.Id}", resultDto);
+            //return NoContent();
+            return Created($"/tasks/{resultDto.Id}", resultDto);
         }
         catch (ArgumentException ex)
         {
